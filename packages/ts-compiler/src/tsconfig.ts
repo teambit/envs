8288f853ebd46f2321c7555f264b1978040806be
Doron Tsur <qballer@gmail.com>
2019-11-17 12:11:47 +0200

    remove comment

--- packages/ts-compiler/src/tsconfig.ts
+++ packages/ts-compiler/src/tsconfig.ts
@@ -38,24 +38,3 @@ export function getTSConfig(isDev: boolean, overrideConfig: GenericObject) {
   return merge({}, defaultOptions, overrideConfig);
 }
 
-/*
-export default {
-  "compilerOptions": {
-      "target": "es2015",
-      "skipLibCheck": true,
-      "esModuleInterop": true,
-      "allowSyntheticDefaultImports": true,
-      "strict": false,
-      "forceConsistentCasingInFileNames": true,
-      "module": "CommonJS",
-      "sourceMap": true,
-      "moduleResolution": "Node",
-      "resolveJsonModule": true,
-      "isolatedModules": false,
-      "declaration": false,
-      "experimentalDecorators": true,
-      "jsx": "react",
-      "rootDir": './'
-  }
-}
-*/
