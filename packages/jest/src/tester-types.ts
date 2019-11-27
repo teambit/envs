2f0fe1f0a87146eadf15eb62d944f066e23e2501
Doron Tsur <qballer@gmail.com>
2019-11-27 12:20:27 +0200

    wierd bug

--- packages/jest/src/tester-types.ts
+++ packages/jest/src/tester-types.ts
@@ -3,7 +3,3 @@ import { CompilerContext, Compiler } from '@bit/bit.envs.common.compiler-types';
 export interface TesterReturnType {}
 
 export interface TesterContext extends CompilerContext {}
-
-export interface Tester extends Compiler {
-  action: (ctx: TesterContext) => Promise<TesterReturnType>;
-}
