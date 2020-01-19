0e43f67d894c79a7cf7a20cdb926e1d60acbff83
Josh Kuttler <yoshuakuttler@gmail.com>
2020-01-19 17:17:28 +0200

    update name of tests

--- packages/ts-compiler/test/preset.spec.ts
+++ packages/ts-compiler/test/preset.spec.ts
@@ -2,15 +2,15 @@ import { expect } from 'chai';
 import { typeScriptPreset } from '../src/typescript-preset';
 
 describe('TypeScript Preset', function() {
-  it('getDynamicConfig function needs to be returned from TypeScript Preset', function() {
+  it('should return getDynamicConfig function from TypeScript Preset', function() {
     const preset = typeScriptPreset.getDynamicConfig;
     expect(preset).to.exist;
   });
-  it('compilerPath needs to be returned from getDynamicConfig function', function() {
+  it('should return compilerPath from getDynamicConfig function', function() {
     const preset = typeScriptPreset.getDynamicConfig ? typeScriptPreset.getDynamicConfig() : {};
     expect(preset.compilerPath).to.equal('typescript/bin/tsc');
   });
-  it('configFileName needs to be returned from getDynamicConfig function', function() {
+  it('should return configFileName from getDynamicConfig function', function() {
     const preset = typeScriptPreset.getDynamicConfig ? typeScriptPreset.getDynamicConfig() : {};
     expect(preset.configFileName).to.equal('tsconfig.json');
   });
