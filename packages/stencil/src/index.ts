7b53c7859f76c88e25cea15afc893729bd832d36
tallyb <tally.barak@gmail.com>
2020-03-27 06:36:45 +0300

    Stencil wip

--- packages/stencil/src/index.ts
+++ packages/stencil/src/index.ts
@@ -1,50 +1,66 @@
+import Vinyl from 'vinyl';
 import * as path from 'path';
 import debug from 'debug';
-import * as fs from 'fs-extra';
+import { promises as fsp } from 'fs';
 
 import { compile } from '@stencil/core/compiler/stencil.js';
 
-(async function() {
-  const filePath = path.join(
-    process.cwd(),
-    '../workspace/components/my-component/components/my-component/my-component.tsx'
-  );
-  console.log(filePath);
-})();
+import { TSConfig } from './tsconfig';
 
 import {
   CompilerContext,
-  ActionReturnType,
+  BuildResults,
   createCapsule,
   destroyCapsule,
-  readFiles
-} from '@bit/bit.envs.common.utils';
+  readFiles,
+  getTestFiles,
+  getSourceFiles,
+} from '@bit/bit.envs.compilers.utils';
 
 if (process.env.DEBUG) {
   debug('build');
 }
 
-export function getDynamicPackageDependencies() {
-  return {};
+const DEV_DEPS = {};
+
+export function getDynamicPackageDependencies(): Record<string, any> {
+  return {
+    devDependencies: DEV_DEPS,
+  };
 }
 
-export function getDynamicConfig(ctx: CompilerContext) {
+export function getDynamicConfig(ctx: CompilerContext): Record<string, any> {
   return ctx.rawConfig;
 }
 
-export async function action(ctx: CompilerContext): Promise<ActionReturnType> {
-  const { context } = ctx;
-  const { componentObject, isolate } = context;
-
+export async function action(ctx: CompilerContext): Promise<BuildResults> {
   // build capsule
-  const { res, directory } = await createCapsule(isolate, { shouldBuildDependencies: true });
+  const { res, directory } = await createCapsule(ctx.context.isolate, { shouldBuildDependencies: true });
   const distDir = path.join(directory, 'dist');
 
+  const componentObject = res.componentWithDependencies.component.toObject();
+  const { files, mainFile } = componentObject;
+
+  debug(`Building capsule in ${directory}`);
+
+  // create tsconfig.json
+  let tests: Vinyl[] = getTestFiles(files, []);
+  let TS = Object.assign(TSConfig, {
+    exclude: [...TSConfig.exclude, ...tests.map((s: Vinyl): string => s.path)],
+  });
+
+  const TSFile = path.resolve(directory, 'tsconfig.json');
+  await fsp.writeFile(TSFile, JSON.stringify(TS, null, 4));
   try {
-    const source = await fs.readFile(path.join(directory, componentObject.mainFile));
-    let res = await compile(source.toString());
-    await fs.ensureDir(distDir);
-    await fs.writeFile(path.join(distDir, 'comp.js'), res.code);
+    console.log('MAIN', mainFile);
+    console.log('DIST', distDir);
+    console.log('FILES', files);
+    files.forEach((f) => console.log(f.basename));
+    const main = files.find((f: Vinyl): boolean => f.path === mainFile);
+    let res = await compile(main.contents.toString(), { module: 'cjs' });
+    console.log('RES', res);
+    await fsp.mkdir(distDir);
+    await fsp.writeFile(path.resolve(distDir, `${main.stem}.js`), res.code);
   } catch (e) {
     console.log(e);
     process.exit(1);
@@ -55,6 +71,6 @@ export async function action(ctx: CompilerContext): Promise<ActionReturnType> {
   destroyCapsule(res.capsule);
   return {
     mainFile: `${componentObject.name}.common.js`,
-    dists: dists || []
+    dists: dists || [],
   };
 }
