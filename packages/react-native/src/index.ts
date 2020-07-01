c46835defae2d04761a10a73d3d4d9224f058fed
Josh Kuttler <yoshuakuttler@gmail.com>
2020-07-01 19:48:55 +0300

    update dev dependencies, and deleted old files in react native compiler

--- packages/react-native/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { TypescriptCompiler } from '@bit/bit.envs.compilers.typescript';
-import { reactNativePreset } from './reactnative-preset';
-
-export default new TypescriptCompiler(reactNativePreset);
